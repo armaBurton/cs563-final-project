cee69a46b856a638abab795805835daf9c6aa517
Arma Burton <arma@armaburton.com>
2025-03-15 17:59:15 -0700

    memory game is finished

--- gamestate.js
+++ gamestate.js
@@ -1,67 +1,78 @@
 //gamestate.js
 const chooser = [];
 const matches = [];
+const gameArr = [];
 
 export const gamestate = async (randomizedGameArr, innerDiv, square) => {
+  randomizedGameArr.map((rga) => gameArr.push(rga));
   if (square) {
     if (chooser[0]?.innerDiv.id != innerDiv.id) {
       chooser.push({ innerDiv, square });
-      toggleVisibility(innerDiv, square);
+      toggleVisibility(innerDiv);
     }
 
     if (chooser.length === 2) {
       //   console.log(chooser[0], chooser[1].innerDiv.id);
 
-      await togglePointerEvents();
+      togglePointerEvents();
+      console.log(chooser[0].square.dataset.name);
+      console.log(chooser[1].square.dataset.name);
 
-      //   console.log("Time Out");
       await delay(2000);
-      //   console.log("Time In");
 
       if (chooser[0].square.dataset.name === chooser[1].square.dataset.name) {
-        await handleMatch(chooser);
+        handleMatch(chooser);
       }
-      await togglePointerEvents();
-      //   console.log(chooser[0].square.dataset.name);
-      //   console.log(chooser[1].square.dataset.name);
-      chooser.forEach((c) => toggleVisibility(c.innerDiv, c.square));
+      togglePointerEvents();
+      toggleMatchPointers();
+      chooser.forEach((c) => {
+        if (!c.square.classList.contains("match")) toggleVisibility(c.innerDiv);
+      });
       purgeArr(chooser);
     }
   }
-  //   console.log(randomizedGameArr);
 };
 
-const handleMatch = async (chooser) => {
-  chooser.forEach(async (c) => {
-    // console.log(c);
-    await c.innerDiv.classList.add("match");
+const toggleMatchPointers = () => {
+  matches.forEach((id) => {
+    document.getElementById(id).style.pointerEvents = "none";
   });
 };
 
-const togglePointerEvents = async () => {
-  //   const gridUnits = document.querySelectorAll(".grid-unit");
-  //   gridUnits.forEach((el) => {
-  //     if (!el.classList.contains("match")) {
-  //       //   console.log(el.classList);
-  //       //   console.log("No Match");
-  //       const bit = window.getComputedStyle(el);
-  //       console.log(bit)
-  //       //   console.log(currentPointerEvents);
-  //       currentPointerEvents === "none"
-  //         ? (el.style.pointerEvents = "auto")
-  //         : (el.style.pointerEvents = "none");
-  //     }
-  //   });
+const handleMatch = (chooser) => {
+  chooser.forEach((c) => {
+    c.square.classList.add("match");
+    matches.push(c.square.id);
+  });
+
+  if (matches.length === 20) {
+    winState();
+  }
 };
 
-export const toggleVisibility = (innerDiv, square) => {
-  //   console.log("innerDiv: ", innerDiv.classList);
+const togglePointerEvents = () => {
+  console.log("Matches[]:", matches);
+
+  for (let i = 0; i < gameArr.length; i++) {
+    const id = `b${i}`;
+    const tile = document.getElementById(id);
+
+    if (tile?.style && !tile.classList.contains("match")) {
+      tile.style.pointerEvents === "none"
+        ? (tile.style.pointerEvents = "auto")
+        : (tile.style.pointerEvents = "none");
+    }
+  }
+};
+
+export const toggleVisibility = (innerDiv) => {
   if (!innerDiv.classList.contains("match")) {
     innerDiv.style.opacity === "0"
       ? (innerDiv.style.opacity = "1")
       : (innerDiv.style.opacity = "0");
+  } else {
+    console.log("Match Found in Toggle Visibility");
   }
-  //   console.log("toggleVisibility: ", square.dataset.name);
 };
 
 const delay = (ms) => new Promise((res) => setTimeout(res, ms));
@@ -70,4 +81,24 @@ const purgeArr = (arr) => {
   while (arr.length > 0) {
     arr.pop();
   }
+};
+
+const winState = () => {
+  const winModal = document.getElementById("win-state");
+  const newGame = document.getElementById("new-game");
+  const quit = document.getElementById("quit");
+  matches.length === 20
+    ? (winModal.style.visibility = "visible")
+    : (winModal.style.visibility = "hidden");
+
+  newGame.addEventListener("click", () => {
+    window.location.reload(true);
+  });
+
+  quit.addEventListener("click", () => {
+    winModal.style.visibility = "hidden";
+  });
+  //   newGame.addEventListener("click", window.location.reload(true));
+
+  //   quit.addEventListener("click", (winModal.style.visibility = "hidden"));
 };
